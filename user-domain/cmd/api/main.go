2df47b7d8b4646e69bab6db5af9a6a35610fa8e6
son-vp1998 <son-vp@genkisystem.com>
2025-10-02 21:17:31 +0700

    refactor router

--- user-domain/cmd/api/main.go
+++ user-domain/cmd/api/main.go
@@ -4,10 +4,14 @@ import (
 	"net/http"
 	"user-domain/controller/handler"
 	usercontroler "user-domain/controller/user"
+	"user-domain/internal/outport"
 	"user-domain/internal/service"
 	database "user-domain/pkg/db"
 	"user-domain/pkg/logger"
 	userrepo "user-domain/repository/user"
+
+	"github.com/go-chi/chi/v5"
+	"gorm.io/gorm"
 )
 
 type Server struct {
@@ -23,18 +27,31 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
-	userRepo := userrepo.NewUserRepo(gorm)
 	logger := logger.NewLogger()
-	userService := service.NewUserService(userRepo, logger)
-	userControler := usercontroler.NewUserControler(userService, logger)
-	handler := handler.Handler(userControler)
+	r := chi.NewRouter()
+	buidRouter(r, gorm, logger)
 	s := Server{
 		httpServer: &http.Server{
-			Handler: handler,
+			Handler: r,
 		},
 	}
 	err = s.httpServer.ListenAndServe()
 	if err != nil {
 		panic("error running server")
 	}
+}
+
+func buildUserSubRouter(r chi.Router, db *gorm.DB, logger outport.Logger) {
+	userRepo := userrepo.NewUserRepo(db)
+	userService := service.NewUserService(userRepo, logger)
+	userControler := usercontroler.NewUserControler(userService, logger)
+	handler.HandlerWithOptions(userControler, handler.ChiServerOptions{
+		BaseRouter: r,
+	})
+}
+
+func buidRouter(r chi.Router, db *gorm.DB, logger outport.Logger) {
+	r.Route("/api/v1", func(r chi.Router) {
+		buildUserSubRouter(r, db, logger)
+	})
 }

06bf27cd6319b5a69889d4caa51175b21fae1615
son-vp1998 <son-vp@genkisystem.com>
2025-10-14 15:27:01 +0700

    add code

--- user-domain/internal/application/inbound/user.go
+++ user-domain/internal/application/inbound/user.go
@@ -8,6 +8,6 @@ type UserApi interface {
 	PostUsers(w http.ResponseWriter, r *http.Request)
 	PutUsersUserId(w http.ResponseWriter, r *http.Request, userID string)
 	GetUsersUserId(w http.ResponseWriter, r *http.Request, userID string)
-	// List(offset int, limit int) (*dto.UsersResponse, error)
 	DeleteUsersUserId(w http.ResponseWriter, r *http.Request, userId string)
+	// List(offset int, limit int) (*dto.UsersResponse, error)
 }

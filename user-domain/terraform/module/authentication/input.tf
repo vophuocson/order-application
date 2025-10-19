91e20278d652642f304a3cfdf04be59551b1751f
son-vp1998 <son-vp@genkisystem.com>
2025-10-19 14:35:12 +0700

    add security module

---
+++ user-domain/terraform/module/authentication/input.tf
@@ -0,0 +1,12 @@
+data "tls_certificate" "github" {
+  url = "https://token.actions.githubusercontent.com"
+}
+
+variable "allowed_repos_branches" {
+  description = "GitHub repos/branches allowed to assume the IAM role."
+  type = list(object({
+    org = string
+    repo = string
+    branche = string
+  }))
+}

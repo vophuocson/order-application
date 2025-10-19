58357720ee9d09b9849aa9dd63d196087422b9de
son-vp1998 <son-vp@genkisystem.com>
2025-10-19 16:37:42 +0700

    add db module

---
+++ user-domain/terraform/module/storage/postgres/input.tf
@@ -0,0 +1,27 @@
+variable "engine_type" {
+  type = string
+  description = "the engine runing db"
+}
+
+variable "storage_size" {
+  type = number
+  description = "size of storage db"
+}
+
+variable "instance_class" {
+  type = string
+  description = "the class  that instance using"
+}
+
+variable "skip_final_snapshot" {
+  type = bool
+  default = true
+}
+
+data "aws_secretsmanager_secret_version" "creds" {
+  secret_id = "db-creds"
+}
+
+locals {
+  db_cres = jsondecode(data.aws_secretsmanager_secret_version.creds.secret_string)
+}
---
+++ user-domain/terraform/module/storage/postgres/main-db-instance.tf
@@ -0,0 +1,9 @@
+resource "aws_db_instance" "db" {
+  identifier_prefix =  "db"
+  engine = var.engine_type
+  allocated_storage = var.storage_size
+  instance_class = var.instance_class
+  skip_final_snapshot = var.skip_final_snapshot
+  username = local.db_cres.username
+  password = local.db_cres.password
+}

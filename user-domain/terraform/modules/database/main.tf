372cddd20691139fa82b703705140a5613dc2053
son-vp1998 <son-vp@genkisystem.com>
2025-10-27 18:53:18 +0700

    remove unnesesary code

--- user-domain/terraform/modules/database/main.tf
+++ user-domain/terraform/modules/database/main.tf
@@ -1,13 +1,6 @@
 locals {
   name = "${var.project_name}-${var.environment}"
 }
-resource "aws_secretsmanager_secret_version" "db_password" {
-  secret_id = aws_secretsmanager_secret.db_password.id
-  secret_string = jsonencode({
-    username = var.master_username
-    password = random_password.db_password.result
-  })
-}
 
 # Security Group for RDS
 resource "aws_security_group" "rds" {

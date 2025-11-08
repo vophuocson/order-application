efbc53e708ad96b54cdd314323c167710256ae7d
son-vp1998 <son-vp@genkisystem.com>
2025-11-08 16:24:20 +0700

    add func create new user payment

--- user-domain/internal/domain/command/user_updated_publicer.go
+++ user-domain/internal/domain/command/user_updated_publicer.go
@@ -19,3 +19,11 @@ func (uP *UpdationUserPayment) Execute(ctx context.Context) error {
 func (uP *UpdationUserPayment) Undo(ctx context.Context) error {
 	return uP.producer.Push(ctx, "channel name", uP.compensateUser)
 }
+
+func NewUpdationUserPayment(producer outport.Producer, commandUser *entity.User, compensateUser *entity.User) Command {
+	return &UpdationUserPayment{
+		producer:       producer,
+		commandUser:    commandUser,
+		compensateUser: compensateUser,
+	}
+}

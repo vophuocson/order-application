552ff2e52d3a7445a8f43fb473a5fd1b906592eb
son-vp1998 <son-vp@genkisystem.com>
2025-11-22 19:11:15 +0700

    fix syntax error

---
+++ user-domain/internal/application/orchestrator/workflow/logging_observer.go
@@ -0,0 +1,28 @@
+package workflow
+
+import (
+	"user-domain/internal/application/orchestrator/workflow/observer"
+	"user-domain/internal/application/outbound"
+)
+
+// LoggingObserver implements WorkflowObserver for logging workflow events
+type LoggingObserver struct {
+	logger outbound.Logger
+}
+
+// NewLoggingObserver creates a new logging observer
+func NewLoggingObserver(logger outbound.Logger) observer.WorkflowObserver {
+	return &LoggingObserver{
+		logger: logger,
+	}
+}
+
+// OnEvent handles workflow events by logging them
+func (o *LoggingObserver) OnEvent(event *observer.WorkflowEvent) {
+
+	if event.Error != nil {
+		o.logger.Error("workflow_log", event)
+	} else {
+		o.logger.Info("workflow_log", event)
+	}
+}
